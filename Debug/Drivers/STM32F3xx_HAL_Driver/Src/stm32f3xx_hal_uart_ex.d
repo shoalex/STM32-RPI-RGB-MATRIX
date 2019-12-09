1f463231f5359e06e1e96f9aad3dc60eebf709ac
Alex Shoyhit <shoalex@gmail.com>
2019-12-09 12:47:59 +0200

    first commit

---
+++ Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.d
@@ -0,0 +1,86 @@
+Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.o: \
+ ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart_ex.c \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Inc/stm32f3xx_hal_conf.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cm4.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h \
+ /home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Inc/stm32f3xx_hal_conf.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h:
+
+/home/alex/STM32Projects/RPI-RGB-MATRIX/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h:
---
+++ Inc/RPI-RGB-MATRIX.h
@@ -0,0 +1,15 @@
+/*
+ * RPI-RGB-MATRIX.h
+ *
+ *  Created on: Dec 9, 2019
+ *      Author: alex
+ */
+
+#ifndef RPI_RGB_MATRIX_H_
+#define RPI_RGB_MATRIX_H_
+#include "main.h"
+
+
+void WriteWord(unsigned char* Word[],int arrSize);
+
+#endif /* RPI_RGB_MATRIX_H_ */
